71bc2ac88ff44c2618b1dc061efe34de2e17580c
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2023-12-23 18:14:30 +0000

    First solution

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,22 @@
-def caesar_cipher
-	"Hello"
+def caesar_cipher(string, num)
+	ascii_array = string.split("").map do |char|
+		if (char.ord > 64 && char.ord < 91) || (char.ord > 96 && char.ord < 123)
+			char.ord
+		else
+			char
+		end
+	end
+	word = ascii_array.map do |ascii|
+		if (ascii.ord > 64 && ascii.ord < 91) || (ascii.ord > 96 && ascii.ord < 123)
+			(ascii + num).chr
+		else
+			ascii
+		end
+	end
+	word.join("")
 end
 
-puts caesar_cipher
+p caesar_cipher("What a string!", 5)
+
+# caesar_cipher("What a string!", 5)
+#Answer: "Bmfy f xywnsl!"
